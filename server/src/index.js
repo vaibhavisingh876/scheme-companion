6467265f7088a57992589e382086be616abd965c
Vaibhavi Singh <vaibhavisinghhh87@gmail.com>
2026-08-10 00:57:11 +0530

    more changes

--- server/src/index.js
+++ server/src/index.js
@@ -45,6 +45,7 @@ const app = express();
 app.set("trust proxy", 1);
 
 const allowedOrigins = [
+  "https://scheme-companion.vercel.app",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:3000",
